36bf71892293450713664c2cde0b4b088418a61d
zerozet <catyx2292000@gmail.com>
2024-10-15 02:59:15 +0630

    save
    save

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  productionBrowserSourceMaps: true, // Enable source maps for production
+
   images: {
     remotePatterns: [
       {
