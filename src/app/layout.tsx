d14040fb7d2e0e1fc5fad6254cec0ece1b6280db
zerozet <catyx2292000@gmail.com>
2024-10-15 01:06:12 +0630

    save
    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,37 @@ import React from "react";
 import SettingsProvider from "@/providers/SettingsProvider";
 import SettingService from "@/services/SettingService";
 import AntdStyleRegistry from "@/components/AntdStyleRegistry";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 const AppLayout: React.FC<{ children: React.ReactNode }> = async ({
   children,
 }) => {
   const settings = await new SettingService().getPublicSettings();
 
+  if (!settings) {
+    return (
+      <html lang="en">
+        <body
+          style={{
+            margin: 0,
+            padding: 0,
+            width: "100%",
+            minHeight: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <LoadingSpinner /> {/* Display loading spinner */}
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
       <head>
         <title>{settings?.siteName || "Online Learning App"}</title>
-
         <link rel="icon" href="/images/favicon.ico" />
       </head>
       <body
